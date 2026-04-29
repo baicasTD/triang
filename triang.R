c133b9c40ac75a1fd81c5a93a1d92f7118328f4e
baicas <baicas@alumni.uv.es>
2026-04-29 16:44:25 +0200

    Add triang R file and respective functions from wiki/triangular_distribution

---
+++ triang.R
@@ -0,0 +1,65 @@
+dtriang <- function(x, min, max, mode) {
+
+  if (min >= max) stop("min must be < max")
+  if (mode < min || mode > max) stop("mode must be in [min, max]")
+
+  a <- min
+  b <- max
+  c <- mode
+
+  dens <- ifelse(x < a | x > b, 0,
+                 ifelse(x <= c,
+                        2 * (x - a) / ((b - a) * (c - a)),
+                        2 * (b - x) / ((b - a) * (b - c))
+                 ))
+
+  return(dens)
+}
+
+ptriang <- function(q, min, max, mode) {
+
+  if (min >= max) stop("min must be < max")
+  if (mode < min || mode > max) stop("mode must be in [min, max]")
+
+  a <- min
+  b <- max
+  c <- mode
+
+  F <- ifelse(q < a, 0,
+              ifelse(q <= c,
+                     ((q - a)^2) / ((b - a) * (c - a)),
+                     ifelse(q <= b,
+                            1 - ((b - q)^2) / ((b - a) * (b - c)),
+                            1)))
+
+  return(F)
+}
+
+qtriang <- function(p, min, max, mode) {
+
+  if (min >= max) stop("min must be < max")
+  if (mode < min || mode > max) stop("mode must be in [min, max]")
+  if (any(p < 0 | p > 1)) stop("p must be in [0,1]")
+
+  a <- min
+  b <- max
+  c <- mode
+
+  pc <- (c - a) / (b - a)
+
+  q <- ifelse(p <= pc,
+              a + sqrt(p * (b - a) * (c - a)),
+              b - sqrt((1 - p) * (b - a) * (b - c)))
+
+  return(q)
+}
+
+rtriang <- function(n, min, max, mode) {
+
+  if (n <= 0) stop("n must be > 0")
+
+  u <- runif(n)
+  x <- qtriang(u, min, max, mode)
+
+  return(x)
+}
